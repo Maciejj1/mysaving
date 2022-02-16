3cc2ac969274d68123d83b81f2fdd30c2ec0fd93
Maciej Wawryszuk <89411648+Maciejj1@users.noreply.github.com>
2022-02-16 11:25:39 +0100

    MYS-4

--- src/App.js
+++ src/App.js
@@ -6,6 +6,18 @@ import RegisterForm from './components/RegisterForm/RegisterForm';
 import {BrowserRouter as Router, Route , Routes , } from 'react-router-dom'
 import Home from './components/Home/Home';
 class App extends Component {
+
+
+
+
+
+
+
+
+
+
+
+  
   render(){
   return (
     <div className="App">
