230fa63085cf4640b52dc77b197eed4fafcc59ad
Maciej Wawryszuk <89411648+Maciejj1@users.noreply.github.com>
2022-02-24 19:45:50 +0100

    :P

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import LoginForm from "./components/LoginForm/LoginForm";
 import RegisterForm from "./components/RegisterForm/RegisterForm";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Home from "./components/Home/Home";
+import { auth, db } from "./components/FirebaseConfig/Config";
 import Expenses from "./components/Expenses/Expenses";
 import Settings from "./components/SettingsPage/Settings";
 import Notifications from "./components/Notifications/Notifications";
